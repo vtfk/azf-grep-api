0a084aac391b3ca223aa467e18d57433795515e4
Mats Andreassen <matsand@live.no>
2020-11-20 22:02:11 +0100

    La til metode for incasesensitiv sjekk

--- GetUtdanningsprogrammer/index.js
+++ GetUtdanningsprogrammer/index.js
@@ -3,16 +3,13 @@ const getResponseObject = require('../lib/get-response-object')
 
 const utdanningsprogrammer = require('../data/data.json')
 
-const removeProgramomraader = ({ programomraader, ...rest }) => {
-  return { ...rest }
-}
-
 module.exports = async function (context, req) {
   logConfig({ prefix: context.invocationId, localLogger: context.log })
-  const { returnChildrens } = req.query
+  const { full } = req.query
+  const returnFull = full.toLowerCase() === 'true'
 
-  logger('info', ['get-utdanningsprogram', 'returning utdanningsprogrammer', (returnChildrens ? 'return all childs' : 'remove programomraader')])
+  logger('info', ['get-utdanningsprogram', 'returning utdanningsprogrammer', (returnFull ? 'full objects ' : 'remove trinn')])
 
-  if (returnChildrens) return getResponseObject(utdanningsprogrammer)
-  return getResponseObject([...utdanningsprogrammer].map(({ trinn, ...rest }) => ({ ...rest, trinn: [...trinn].map(removeProgramomraader) })))
+  if (returnFull) return getResponseObject(utdanningsprogrammer)
+  return getResponseObject([...utdanningsprogrammer].map(({ trinn, ...rest }) => ({ ...rest, trinn: undefined })))
 }
